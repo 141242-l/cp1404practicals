179c501615c4568bffffdd7ab237b1a4cc645715
linn <yan.lin@my.jcu.edu.au>
2025-11-09 21:25:08 +0800

    Save the list of guitars to a CSV file

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -12,3 +12,9 @@ def load_guitars(filename):
             cost = float(parts[2])
             guitars.append(Guitar(name, year, cost))
     return guitars
+
+def save_guitars(filename, guitars):
+    with open(filename, "w") as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
+
