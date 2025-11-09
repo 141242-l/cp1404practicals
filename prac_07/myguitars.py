0c320f1552a94208be653fa03f18949e42225444
linn <yan.lin@my.jcu.edu.au>
2025-11-09 21:23:48 +0800

    Load guitars from a CSV file

---
+++ prac_07/myguitars.py
@@ -0,0 +1,14 @@
+from guitar import Guitar
+
+FILENAME = "guitars.csv"
+
+def load_guitars(filename):
+    guitars = []
+    with open(filename, "r") as in_file:
+        for line in in_file:
+            parts = line.strip().split(',')
+            name = parts[0]
+            year = int(parts[1])
+            cost = float(parts[2])
+            guitars.append(Guitar(name, year, cost))
+    return guitars
