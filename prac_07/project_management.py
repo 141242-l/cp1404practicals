250cab9db8cfd896b53f73f16de8927aded87650
linn <yan.lin@my.jcu.edu.au>
2025-11-09 21:43:44 +0800

    display project

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -13,3 +13,21 @@ def load_projects(filename):
                 name, start_date, priority, cost, completion = parts
                 projects.append(Project(name, start_date, int(priority), float(cost), int(completion)))
     return projects
+
+def save_projects(filename, projects):
+    with open(filename, "w") as file:
+        file.write("Name\tStart Date\tPriority\tCost Estimate\tCompletion Percentage\n")
+        for p in projects:
+            file.write(f"{p.name}\t{p.start_date.strftime('%d/%m/%Y')}\t{p.priority}\t{p.cost_estimate}\t{p.completion_percentage}\n")
+
+def display_projects(projects):
+    incomplete = []
+    complete = []
+
+    for project in projects:
+        if project.is_complete():
+            complete.append(project)
+        else:
+            incomplete.append(project)
+
+
