18f76c1ab844f95c05bc214208ff2b64adf10a95
linn <yan.lin@my.jcu.edu.au>
2025-11-14 19:23:19 +0800

    edit - add name list

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -2,15 +2,8 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
 
-class DynamicLabelsApp(App):
+
+class NameListApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.names = ["Alice", "Bob", "Charlie", "Diana", "Ethan"]
-
-    def build(self):
-        self.root = Builder.load_file('dynamic_labels.kv')
-        main_layout = self.root.ids.main
-
-        for name in self.names:
-            label = Label(text=name)
-            main_layout.add_widget(label)
+        self.names = ["Alice", "Bob", "Charlie", "David"]
