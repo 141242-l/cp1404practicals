13b890033d766b2aa3c368ff3c0cd76806c03c7f
linn <yan.lin@my.jcu.edu.au>
2025-11-14 18:53:47 +0800

    handle up/down button press, update the text input with new value

--- prac_08/convert_m_to_km_solution.py
+++ prac_08/convert_m_to_km_solution.py
@@ -29,3 +29,8 @@ class MilesConverterApp(App):
         km = miles * MILES_TO_KM
         self.output_text = f"{km:.5f}"
 
+ def handle_increment(self, change):
+        miles = self.get_validated_miles()
+        miles += change
+        self.root.ids.input_miles.text = str(miles)
+        self.handle_calculate()
