d5724323ede420d62bff209547db0f8975128e36
linn <yan.lin@my.jcu.edu.au>
2025-11-14 19:05:36 +0800

    the dictionary is defined in the init function

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -6,3 +6,7 @@ class DynamicLabelsApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.names = ["Alice", "Bob", "Charlie", "Diana", "Ethan"]
+
+    def build(self):
+        self.root = Builder.load_file('dynamic_labels.kv')
+        main_layout = self.root.ids.main
