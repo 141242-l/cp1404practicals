3d229f04e07c19ecf1522256e41f5084c2d7be79
linn <yan.lin@my.jcu.edu.au>
2025-11-14 19:03:04 +0800

    add a list of names

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -2,3 +2,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.label import Label
 
+class DynamicLabelsApp(App):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.names = ["Alice", "Bob", "Charlie", "Diana", "Ethan"]
