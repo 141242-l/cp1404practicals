d736112172da4e0d4477f85bf1474ffd78c60ffb
linn <yan.lin@my.jcu.edu.au>
2025-11-14 18:52:00 +0800

    definition

--- prac_08/convert_m_to_km_solution.py
+++ prac_08/convert_m_to_km_solution.py
@@ -6,7 +6,19 @@ Lindsay Ward, IT@JCU
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 __author__ = 'Lindsay Ward'
 
 MILES_TO_KM = 1.60934
+
+
+class MilesConverterApp(App):
+    """ MilesConverterApp is a Kivy App for converting miles to kilometres """
+    output_text = StringProperty()
+    def build(self):
+        """ build the Kivy app from the kv file """
+        self.title = "Convert Miles to Kilometres"
+        self.output_text = "0.0"
+        self.root = Builder.load_file('convert_miles_km.kv')
+        return self.root
