735091217ba672eccaa940d2f3cf871672877023
linn <yan.lin@my.jcu.edu.au>
2025-11-14 19:24:58 +0800

    load

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -7,3 +7,6 @@ class NameListApp(App):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.names = ["Alice", "Bob", "Charlie", "David"]
+
+    def build(self):
+        self.root = Builder.load_file('dynamic_labels.kv')
