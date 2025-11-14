c7fb66247ec491219ca82eb9391486946c86035a
linn <yan.lin@my.jcu.edu.au>
2025-11-14 19:55:40 +0800

    set dynamic labels

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -10,3 +10,12 @@ class NameListApp(App):
 
     def build(self):
         self.root = Builder.load_file('dynamic_labels.kv')
+
+        main_layout = self.root.ids.main
+
+        for name in self.names:
+            lbl = Label(text=name, font_size=24)
+            main_layout.add_widget(lbl)
+        return self.root
+
+NameListApp().run()
