9fede1986c42e4c46785589b3fd0e1fb6869cf4e
linn <yan.lin@my.jcu.edu.au>
2025-11-18 14:28:39 +0800

    format

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -4,3 +4,7 @@ def test_silver_service_taxi():
     taxi = SilverServiceTaxi("Hummer", 200, 2)
 
     print(taxi)
+
+    taxi.drive(18)
+    fare = taxi.get_fare()
+    print(f"Fare for 18 km: ${fare:.2f}")
