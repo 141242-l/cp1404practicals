a21df757785a6dc868227248f2c89a6ada1f4b2c
linn <yan.lin@my.jcu.edu.au>
2025-11-18 10:44:49 +0800

    Change the __init__ function

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -8,10 +8,12 @@ from prac_09.car import Car
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
 
-    def __init__(self, name, fuel, price_per_km):
-        """Initialise a Taxi instance, based on parent class Car."""
+    def __init__(self, name, fuel, price_per_km=None):
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
+        if price_per_km is None:
+            self.price_per_km = Taxi.price_per_km
+        else:
+            self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
