847c6c921953f39a12c0fea912d0ab68dbb8d752
linn <yan.lin@my.jcu.edu.au>
2025-11-18 13:49:25 +0800

    Returns the actual distance driven

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -8,3 +8,9 @@ class UnreliableCar(Car):
         """Initialise an UnreliableCar, with reliability (0-100)."""
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        if random.uniform(0, 100) < self.reliability:
+            return super().drive(distance)
+        else:
+            return 0
