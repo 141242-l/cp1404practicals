682a9f9e9bfffb515acee48c0b2d5c77817d3ddb
linn <yan.lin@my.jcu.edu.au>
2025-11-18 12:44:54 +0800

    add

--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -8,6 +8,8 @@ from prac_09.car import Car
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
 
+    price_per_km = 1.23
+
     def __init__(self, name, fuel, price_per_km=None):
         super().__init__(name, fuel)
         if price_per_km is None:
