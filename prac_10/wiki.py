7bfdf33ad5cf988995c2264f36d1cc8873f72e01
linn <yan.lin@my.jcu.edu.au>
2025-11-25 03:29:24 +0800

    complete while loop

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -9,3 +9,10 @@ def main():
             print(f"\n{page.title}")
             print(f"{page.summary}\n")
             print(page.url)
+        except DisambiguationError as e:
+            print("We need a more specific title. Try one of the following, or a new search:")
+            print(e.options)
+        except PageError:
+            print(f'Page id "{title}" does not match any pages. Try another id!')
+        except Exception as e:
+            print(f"An error occurred: {e}")
