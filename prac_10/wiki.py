c53ebad5ec40f6568fca2df88f37415d2c08e304
linn <yan.lin@my.jcu.edu.au>
2025-11-25 03:22:50 +0800

    add while

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -3,3 +3,9 @@ from wikipedia.exceptions import DisambiguationError, PageError
 
 def main():
     title = input("Enter page title: ").strip()
+    while title != "":
+        try:
+            page = wikipedia.page(title, auto_suggest=False)
+            print(f"\n{page.title}")
+            print(f"{page.summary}\n")
+            print(page.url)
